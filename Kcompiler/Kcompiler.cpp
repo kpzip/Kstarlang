f1af98df1eefef70cf91c628f2b87a51f01bc659
kpzip <kpzipgames@gmail.com>
2023-12-28 12:28:43 -0800

    initial commit

---
+++ Kcompiler/Kcompiler.cpp
@@ -0,0 +1,12 @@
+﻿// Kcompiler.cpp : Defines the entry point for the application.
+//
+
+#include "Kcompiler.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
