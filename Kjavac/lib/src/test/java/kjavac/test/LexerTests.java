39f64e79dc9d1ed0999d78f0bfac3837db5daf90
kpzip <kpzipgames@gmail.com>
2024-01-05 17:34:43 -0800

    c

---
+++ Kjavac/lib/src/test/java/kjavac/test/LexerTests.java
@@ -0,0 +1,24 @@
+package kjavac.test;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+
+class LexerTests {
+	
+	public static final Map<String, String> LEXER_TEST_DATA = new HashMap<String, String>();
+	
+	static {
+		
+	}
+	
+	
+    @Test 
+    private void lexerTest() throws TestException {
+		
+    }
+    
+    
+    
+}
---
+++ Kjavac/lib/src/test/java/kjavac/test/TestException.java
@@ -0,0 +1,14 @@
+package kjavac.test;
+
+public class TestException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 2400767937698762898L;
+
+	public TestException(String testName) {
+		super(testName);
+	}
+
+}
